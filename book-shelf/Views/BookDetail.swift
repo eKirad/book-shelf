eff7629bedeeb7106b7f87c1305b22891d8c7c4d
eKirad <evkirad@gmail.com>
2024-11-28 20:57:30 +0200

    :art: Adds Book View

--- book-shelf/Views/BookDetail.swift
+++ book-shelf/Views/BookDetail.swift
@@ -1,15 +1,9 @@
-//
-//  BookDetail.swift
-//  book-shelf
-//
-//  Created by Evgeni Kiradzhiyski on 28.11.24.
-//
-
 import SwiftUI
 
 struct BookDetail: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, book!")
     }
 }
 
