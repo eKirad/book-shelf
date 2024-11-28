9b121deaaba550a1feddab6e89ab54e90c6c89f3
eKirad <evkirad@gmail.com>
2024-11-28 20:58:14 +0200

    :label: Updates Model types

--- book-shelf/Models/Book.swift
+++ book-shelf/Models/Book.swift
@@ -4,4 +4,11 @@ struct Book: Identifiable {
     var id: Int
     var name: String
     var description: String
+    var genre: Genre
+    
+    enum Genre: String, CaseIterable, Codable {
+        case mystery = "mystery"
+        case thriller = "thriller"
+        case romance = "romance"
+    }
 }
--- book-shelf/Models/ModelData.swift
+++ book-shelf/Models/ModelData.swift
@@ -3,16 +3,16 @@ import Foundation
 @Observable
 class ModelData {
     var books: Array<Book> = [
-        Book (id: 12, name: "Test", description: "Test description"),
-        Book (id: 12, name: "Test another", description: "Test description"),
-        Book (id: 12, name: "Okay, this", description: "Test description"),
+        Book (id: 12, name: "Test", description: "Test description", genre: Book.Genre.mystery),
+        Book (id: 12, name: "Test another", description: "Test description", genre: Book.Genre.romance),
+        Book (id: 12, name: "Okay, this", description: "Test description", genre: Book.Genre.thriller),
         
     ]
     
-//    var categories: [String: [Book]] {
-//        Dictionary(
-//            grouping: books,
-//            by: { $0.category.rawValue }
-//        )
-//    }
+    var genres: [String: [Book]] {
+        Dictionary(
+            grouping: books,
+            by: { $0.genre.rawValue }
+        )
+    }
 }
