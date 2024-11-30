82d5ed57ecdc3fb2943726569715e39f8845c9de
eKirad <evkirad@gmail.com>
2024-11-30 13:12:48 +0200

    :lipstick: Adds profile views

---
+++ book-shelf/Profiles/ProfileHost.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileHost.swift
+//  book-shelf
+//
+//  Created by Evgeni Kiradzhiyski on 30.11.24.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileHost()
+}
---
+++ book-shelf/Profiles/ProfileSummary.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileSummary.swift
+//  book-shelf
+//
+//  Created by Evgeni Kiradzhiyski on 30.11.24.
+//
+
+import SwiftUI
+
+struct ProfileSummary: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileSummary()
+}
