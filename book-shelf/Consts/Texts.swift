eaaeacd0c6df9dca8fea5da672fa6759d7de4f1d
eKirad <evkirad@gmail.com>
2024-11-30 13:30:53 +0200

    :label: Extends text consts

--- book-shelf/Consts/Texts.swift
+++ book-shelf/Consts/Texts.swift
@@ -3,4 +3,8 @@ import Foundation
 
 struct Texts {
     static let selectBook = "Select book"
+    static let notifications = "Notifications"
+    static let firstName = "First Name"
+    static let lastName = "Last Name"
+    static let email = "e-mail"
 }
