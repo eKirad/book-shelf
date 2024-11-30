3286bdad20a5a17f4f307a96d65e25819f9d2ebe
eKirad <evkirad@gmail.com>
2024-11-30 14:55:30 +0200

    :bug: Fixes ModelData usage

--- book-shelf/BookShelf.swift
+++ book-shelf/BookShelf.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 @main
 struct book_shelfApp: App {
+    @State private var modelData = ModelData()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(modelData)
         }
     }
 }
