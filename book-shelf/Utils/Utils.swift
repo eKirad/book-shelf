5b6ee01fc9ea8cb854ffd38dd18b84a8f83e783b
eKirad <evkirad@gmail.com>
2024-12-06 23:52:13 +0200

    :art: Extends utils

--- book-shelf/Utils/Utils.swift
+++ book-shelf/Utils/Utils.swift
@@ -1,8 +1,12 @@
-//
-//  Utils.swift
-//  book-shelf
-//
-//  Created by Evgeni Kiradzhiyski on 6.12.24.
-//
+import SwiftUI
 
-import Foundation
+func mapGenreToColor(for genre: BookGenre) -> Color {
+    switch genre {
+    case .mystery:
+        return Color.gray
+    case .thriller:
+        return Color.cyan
+    case .romance:
+        return Color(red: 1.0, green: 0.6, blue: 0.6)
+    }
+}
