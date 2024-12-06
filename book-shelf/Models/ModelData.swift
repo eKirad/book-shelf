c69b273d652bc9486d8a66cae56e8faf8cd4c363
eKirad <evkirad@gmail.com>
2024-12-06 17:18:53 +0200

    :art: Updates Models

--- book-shelf/Models/ModelData.swift
+++ book-shelf/Models/ModelData.swift
@@ -3,10 +3,10 @@ import Foundation
 @Observable
 class ModelData {
     var books: Array<Book> = [
-        Book (id: 12, name: "Test", description: "Test description", genre: BookGenre.mystery, coverImageName: "mystery-genre-cover", authorName: "John Doe"),
-        Book (id: 13, name: "Chek mystery", description: "Test description", genre: BookGenre.mystery, coverImageName: "mystery-genre-cover", authorName:  "Max Musternan"),
-        Book (id: 14, name: "Test another", description: "Test description", genre: BookGenre.romance, coverImageName: "romance-genre-cover", authorName: "Maxi Muster"),
-        Book (id: 15, name: "Okay, this", description: "Test description", genre: BookGenre.thriller, coverImageName: "thriller-genre-cover", authorName: "Johny Doe"),
+        Book (id: 12, name: "Test", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", genre: BookGenre.mystery, coverImageName: "mystery-genre-cover", authorName: "John Doe"),
+        Book (id: 13, name: "Chek mystery", description:  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",genre: BookGenre.mystery, coverImageName: "mystery-genre-cover", authorName:  "Max Musternan"),
+        Book (id: 14, name: "Test another", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", genre: BookGenre.romance, coverImageName: "romance-genre-cover", authorName: "Maxi Muster"),
+        Book (id: 15, name: "Okay, this", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", genre: BookGenre.thriller, coverImageName: "thriller-genre-cover", authorName: "Johny Doe"),
         
     ]
     
