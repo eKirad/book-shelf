ed9df12b673ea8408b4d28aac8a8a410442c44d1
eKirad <evkirad@gmail.com>
2024-12-08 15:03:05 +0200

    :art: Updates views

--- book-shelf/Views/SplashScreen.swift
+++ book-shelf/Views/SplashScreen.swift
@@ -1,12 +1,43 @@
 import SwiftUI
 
-struct SplashView: View {
-    var body: some View {
-       Text("Ok, hi")
-    }
+struct SplashScreen: View {
+   @State private var isGuestUsage = false
     
+    
+    var body: some View {
+        if (isGuestUsage) {
+            ContentView()
+        } else {
+            NavigationView {
+                VStack(alignment: .center) {
+                    NavigationLink(destination: Login()) {
+                        Text(Texts.login)
+                            .bold()
+                            .padding()
+                            .background(Color.cyan)
+                            .foregroundColor(.white)
+                            .cornerRadius(16)
+                    }
+                
+                    NavigationLink(destination: Login()) {
+                        Text(Texts.signUp)
+                            .bold()
+                            .padding()
+                            .background(Color.green)
+                            .foregroundColor(.white)
+                            .cornerRadius(16)
+                    }
+                
+                    Button("Use as guest") {
+                        isGuestUsage.toggle()
+                    }
+                    .padding()
+                }
+            }    
+        }
+    }
 }
 
 #Preview {
-    SplashView()
+    SplashScreen()
 }
