ecacc9a21c3fdf9d445058fe68c1535bfe3109e2
eKirad <evkirad@gmail.com>
2024-12-08 15:02:33 +0200

    :art: Updates Login page

--- book-shelf/Auth/Login.swift
+++ book-shelf/Auth/Login.swift
@@ -1,15 +1,8 @@
-//
-//  Login.swift
-//  book-shelf
-//
-//  Created by Evgeni Kiradzhiyski on 8.12.24.
-//
-
 import SwiftUI
 
 struct Login: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, login")
     }
 }
 
