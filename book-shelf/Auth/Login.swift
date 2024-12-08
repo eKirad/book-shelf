1a1c39a79a903078eec987e41cd1dca4d90304cc
eKirad <evkirad@gmail.com>
2024-12-08 15:02:22 +0200

    :sparkles: Adds login and splash screen

---
+++ book-shelf/Auth/Login.swift
@@ -0,0 +1,18 @@
+//
+//  Login.swift
+//  book-shelf
+//
+//  Created by Evgeni Kiradzhiyski on 8.12.24.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Login()
+}
---
+++ book-shelf/Views/SplashScreen.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+       Text("Ok, hi")
+    }
+    
+}
+
+#Preview {
+    SplashView()
+}
