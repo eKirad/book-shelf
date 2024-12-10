2cc3bb54f5df8eaff3b4591ceb994c56bc5db85b
eKirad <evkirad@gmail.com>
2024-12-10 21:51:15 +0200

    :lipstick: Adds Signup form

---
+++ book-shelf/Auth/Signup.swift
@@ -0,0 +1,18 @@
+//
+//  Signu.swift
+//  book-shelf
+//
+//  Created by Evgeni Kiradzhiyski on 10.12.24.
+//
+
+import SwiftUI
+
+struct Signup: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Signup()
+}
