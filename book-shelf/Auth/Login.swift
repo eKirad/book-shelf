70d1a79286fb55560c4cbfcbad78fb0f9b68e3ef
eKirad <evkirad@gmail.com>
2024-12-10 21:25:30 +0200

    :lipstick: Adds login form

--- book-shelf/Auth/Login.swift
+++ book-shelf/Auth/Login.swift
@@ -1,8 +1,47 @@
 import SwiftUI
 
 struct Login: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var shouldRememberUser: Bool = false
+    @State private var isShowingAlert: Bool = false
+    
     var body: some View {
-        Text("Hello, login")
+        Form {
+            Section(header: Text("Login Credentials")) {
+                TextField("username", text: $username)
+                    .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+                    .disableAutocorrection(true)
+                
+                SecureField("Password", text: $password)
+            }
+            
+            Section {
+                Toggle("Remember me", isOn: $shouldRememberUser)
+            }
+            
+            Section {
+                Button(action: {
+                    handleLogin()
+                }) {
+                    Text("Login")
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.cyan)
+                        .cornerRadius(8)
+                }
+                .listRowBackground(Color.clear)
+            }
+            .padding(.vertical, 10)
+            .padding(.horizontal, 16)
+            .listRowBackground(Color.clear)
+        }
+        .navigationTitle("Login")
+    }
+    
+    private func handleLogin() {
+        isShowingAlert = true
     }
 }
 
