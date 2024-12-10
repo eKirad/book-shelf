21f7cd18e9be20cce800decaee8e28f7ca05be04
eKirad <evkirad@gmail.com>
2024-12-10 21:42:51 +0200

    :art: Formats splash screen code

--- book-shelf/Views/SplashScreen.swift
+++ book-shelf/Views/SplashScreen.swift
@@ -2,35 +2,42 @@ import SwiftUI
 
 struct SplashScreen: View {
    @State private var isGuestUsage = false
-    
+
     var body: some View {
         if (isGuestUsage) {
             ContentView()
         } else {
             NavigationView {
                 VStack(alignment: .center) {
-                    NavigationLink(destination: Login()) {
-                        Text(Texts.login)
-                            .bold()
-                            .padding()
-                            .background(Color.cyan)
-                            .foregroundColor(.white)
-                            .cornerRadius(16)
+                    VStack{
+                        NavigationLink(destination: Login()) {
+                            Text(Texts.login)
+                                .frame(width: 200, alignment: .center)
+                                .bold()
+                                .padding()
+                                .background(Color.cyan)
+                                .foregroundColor(.white)
+                                .cornerRadius(16)
+                        }
+                        
+                        NavigationLink(destination: Login()) {
+                            Text(Texts.signUp)
+                                .frame(width: 200, alignment: .center)
+                                .bold()
+                                .padding()
+                                .background(Color.green)
+                                .foregroundColor(.white)
+                                .cornerRadius(16)
+                        }
+                        
+                        Button(Texts.useAsGuest) {
+                            isGuestUsage.toggle()
+                        }
+                        .padding()
                     }
-                
-                    NavigationLink(destination: Login()) {
-                        Text(Texts.signUp)
-                            .bold()
-                            .padding()
-                            .background(Color.green)
-                            .foregroundColor(.white)
-                            .cornerRadius(16)
-                    }
-                
-                    Button(Texts.useAsGuest) {
-                        isGuestUsage.toggle()
-                    }
-                    .padding()
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 16)
+                    .listRowBackground(Color.clear)
                 }
             }    
         }
