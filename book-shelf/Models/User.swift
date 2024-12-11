b04f02c430fca631e1972b71d4fbb8486ec047ed
eKirad <evkirad@gmail.com>
2024-12-11 18:34:14 +0200

    :label: Update smodels

--- book-shelf/Models/User.swift
+++ book-shelf/Models/User.swift
@@ -1,8 +1,8 @@
-//
-//  User.swift
-//  book-shelf
-//
-//  Created by Evgeni Kiradzhiyski on 11.12.24.
-//
-
 import Foundation
+
+struct User {
+    let id: UUID
+    let username: String
+    let firstName: String
+    let lastName: String
+}
