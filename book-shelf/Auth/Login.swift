122bd037df0bed2c9abe2bcf9c0e79be3cffaf7a
eKirad <evkirad@gmail.com>
2024-12-11 19:54:57 +0200

    :goal_net: Adds form error handling

--- book-shelf/Auth/Login.swift
+++ book-shelf/Auth/Login.swift
@@ -6,8 +6,18 @@ struct Login: View {
     @State private var shouldRememberUser: Bool = false
     @State private var isShowingAlert: Bool = false
     @State private var isLoading: Bool = false
+    @State private var usernameError: String? = nil
+    @State private var passwordError: String? = nil
     @State private var loggedInUser: User? = nil
 
+    private func handleSubmit() {
+        validateFormFields()
+        
+        if (isFormValid()) {
+            handleLogin()
+        }
+    }
+    
     private func handleLogin() {
         isLoading = true
         // TODO: Mock API call
@@ -17,14 +27,61 @@ struct Login: View {
         }
     }
     
+    private func isFormValid() -> Bool {
+        let isUsernameFieldValid = isUsernameValid()
+        let isPasswordFieldValid = isPasswordValid()
+        
+        return isUsernameFieldValid && isPasswordFieldValid
+    }
+    
+    private func isUsernameValid() -> Bool {
+        return ((usernameError?.isEmpty) == nil)
+    }
+    
+    private func isPasswordValid() -> Bool {
+        return ((passwordError?.isEmpty) == nil)
+    }
+    
+    private func validateFormFields() {
+        validateUsername()
+        validatePassword()
+    }
+    
+    private func validateUsername() {
+        if (username.isEmpty) {
+            usernameError = "username cannot be empty"
+        } else {
+            usernameError = nil
+        }
+    }
+    
+    private func validatePassword() {
+        if (password.isEmpty) {
+            passwordError = "Password cannot be empty"
+        } else {
+            passwordError = nil
+        }
+    }
+    
     var loginForm: some View {
         Form {
             Section(header: Text(Texts.loginCredentials)) {
                 TextField(Texts.username, text: $username)
                     .autocapitalization(.none)
                     .disableAutocorrection(true)
+                if let usernameError = usernameError {
+                    Text(usernameError)
+                        .foregroundColor(.red)
+                        .padding(.horizontal)
+                }
                 
                 SecureField(Texts.password, text: $password)
+                
+                if let passwordError = passwordError {
+                    Text(passwordError)
+                        .foregroundColor(.red)
+                        .padding(.horizontal)
+                }
             }
             
             Section {
@@ -33,7 +90,7 @@ struct Login: View {
             
             Section {
                 Button(action: {
-                    handleLogin()
+                    handleSubmit()
                 }) {
                     HStack {
                         isLoading 
