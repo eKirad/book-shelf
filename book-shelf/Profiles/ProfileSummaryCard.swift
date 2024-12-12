a15dc4684c0fad1576b9f3b93602df3a58f88988
eKirad <evkirad@gmail.com>
2024-12-12 14:37:06 +0200

    :recycle: Refactors profile summary card

--- book-shelf/Profiles/ProfileSummaryCard.swift
+++ book-shelf/Profiles/ProfileSummaryCard.swift
@@ -5,27 +5,27 @@ struct ProfileSummaryCard: View {
     let handleSignout: () -> Void
     
     var body: some View {
-            HStack {
-                Image(systemName: "person.fill")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    .foregroundColor(.gray)
-                Text("\(Texts.fullName): \(profile.firstName) \(profile.lastName)")
-            }
-           
-             HStack {
-                Image(systemName: "envelope.fill")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    .foregroundColor(.gray)
-                 Text("\(profile.email)")
-            }
-           
-             HStack {
-                Image(systemName: "bell.fill")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    .foregroundColor(.gray)
-                 Text("\(Texts.notifications): \(profile.areNotificationsActive ? "\(Texts.notificationsOn)": "\(Texts.notificationsOff)")")
-            }
-        
+        HStack {
+            Image(systemName: "person.fill")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .foregroundColor(.gray)
+            Text("\(Texts.fullName): \(profile.firstName) \(profile.lastName)")
+        }
+       
+         HStack {
+            Image(systemName: "envelope.fill")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .foregroundColor(.gray)
+             Text("\(profile.email)")
+        }
+       
+         HStack {
+            Image(systemName: "bell.fill")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .foregroundColor(.gray)
+             Text("\(Texts.notifications): \(profile.areNotificationsActive ? "\(Texts.notificationsOn)": "\(Texts.notificationsOff)")")
+        }
+    
         HStack {
             Button(action: {
                 handleSignout()
