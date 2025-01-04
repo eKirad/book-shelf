704ea380a3b48ffb8bc2a75e0747bce183e5b84a
eKirad <evkirad@gmail.com>
2025-01-04 12:30:57 +0200

    :recycle: Refactors content tab two

--- book-shelf/ContentViewTwo.swift
+++ book-shelf/ContentViewTwo.swift
@@ -4,18 +4,12 @@ struct ContentViewTwo: View {
     @State private var loggedInUser: User? = nil
     @State private var isGuestUser: Bool = false
     
-    
     var body: some View {
         NavigationStack {
             let shouldShowMainScreen = loggedInUser != nil || isGuestUser
             
             if (shouldShowMainScreen) {
-                MainTabTwo(
-                    loggedInUser: $loggedInUser,
-                    isGuestUser: $isGuestUser,
-                    isSignoutLoading: true,
-                    handleSignout: {}
-                )
+                MainTabTwo(loggedInUser: $loggedInUser, isGuestUser: $isGuestUser)
             } else {
                 SplashScreenTwo(loggedInUser: $loggedInUser, isGuestUser: $isGuestUser)
             }
