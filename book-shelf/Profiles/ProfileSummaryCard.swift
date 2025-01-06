611a511a3b1340aadd30b5439c00cd23e9fe41d5
eKirad <evkirad@gmail.com>
2025-01-06 14:21:29 +0200

    :lipstick: Styles profile card properly

--- book-shelf/Profiles/ProfileSummaryCard.swift
+++ book-shelf/Profiles/ProfileSummaryCard.swift
@@ -24,6 +24,9 @@ struct ProfileSummaryCard: View {
                 Form {
                     Section(header: Text("User Information")) {
                         HStack() {
+                            Image(systemName: "person.fill")
+                                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                                .foregroundColor(.gray)
                             LabeledContent("\(Labels.username):") {
                                 TextField(Texts.username, text: Binding(
                                 get: { user.username },
@@ -36,6 +39,9 @@ struct ProfileSummaryCard: View {
                         }
                         
                         HStack {
+                            Image(systemName: "person.fill")
+                                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                                .foregroundColor(.gray)
                             LabeledContent("\(Labels.firstName):") {
                             TextField("First name", text: Binding(
                                 get: { user.firstName},
@@ -48,6 +54,9 @@ struct ProfileSummaryCard: View {
                         }
                         
                         HStack {
+                            Image(systemName: "person.fill")
+                                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                                .foregroundColor(.gray)
                             LabeledContent("\(Labels.lastName):") {
                                 TextField("Last name", text: Binding(
                                     get: { user.lastName},
@@ -60,6 +69,9 @@ struct ProfileSummaryCard: View {
                         }
                       
                         HStack {
+                            Image(systemName: "envelope.fill")
+                                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                                .foregroundColor(.gray)
                             LabeledContent("\(Labels.email):") {
                             TextField("Email", text: Binding(
                                 get: { user.email },
@@ -70,62 +82,49 @@ struct ProfileSummaryCard: View {
                             .disabled(isEditDisabled)
                             }
                         }
+                        
+                        HStack {
+                           Image(systemName: "bell.fill")
+                               .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                               .foregroundColor(.gray)
+                            Toggle(isOn: Binding(
+                               get: { loggedInUser?.areNotificationsActive ?? false },
+                               set: { notificationsToggleValue in
+                                   if loggedInUser != nil {
+                                       loggedInUser?.areNotificationsActive = notificationsToggleValue
+                                   }
+                                }
+                            )) {
+                                Text("\(Texts.notifications): \(loggedInUser?.areNotificationsActive ?? false ? "\(Texts.notificationsOn)": "\(Texts.notificationsOff)")")
+                            }
+                            .disabled(loggedInUser == nil)
+                            .padding(.horizontal)
+                        }
+                        
+                        HStack {
+                            Button(action: {
+                                handleSignout()
+                            }) {
+                                isSignoutLoading
+                                  ? AnyView(ProgressView()
+                                        .font(.title)
+                                        .progressViewStyle(CircularProgressViewStyle())
+                                  )
+                                  : AnyView(HStack {
+                                      Image(systemName: "arrow.right.circle.fill")
+                                          .font(.title)
+                                          .foregroundColor(.gray)
+                                  })
+                
+                                Text(Texts.signOut)
+                                    .foregroundColor(.black)
+                            }
+                       }
+                        
                     }
                 }
             } 
         }
-        //        HStack {
-        //            Image(systemName: "person.fill")
-        //                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-        //                .foregroundColor(.gray)
-        //            Text("\(Texts.fullName): \(loggedInUser?.firstName ?? Texts.nA) \(loggedInUser?.lastName ?? Texts.nA)")
-        //        }
-        //
-        //         HStack {
-        //            Image(systemName: "envelope.fill")
-        //                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-        //                .foregroundColor(.gray)
-        //             Text("\(loggedInUser?.email ?? Texts.nA)")
-        //        }
-        //
-        //         HStack {
-        //            Image(systemName: "bell.fill")
-        //                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-        //                .foregroundColor(.gray)
-        //             Toggle(isOn: Binding(
-        //                get: { loggedInUser?.areNotificationsActive ?? false },
-        //                set: { notificationsToggleValue in
-        //                    if loggedInUser != nil {
-        //                        loggedInUser?.areNotificationsActive = notificationsToggleValue
-        //                    }
-        //                 }
-        //             )) {
-        //                 Text("\(Texts.notifications): \(loggedInUser?.areNotificationsActive ?? false ? "\(Texts.notificationsOn)": "\(Texts.notificationsOff)")")
-        //             }
-        //             .disabled(loggedInUser == nil)
-        //             .padding(.horizontal)
-        //         }
-        //
-        //        HStack {
-        //            Button(action: {
-        //                handleSignout()
-        //            }) {
-        //                isSignoutLoading
-        //                  ? AnyView(ProgressView()
-        //                        .font(.title)
-        //                        .progressViewStyle(CircularProgressViewStyle())
-        //                  )
-        //                  : AnyView(HStack {
-        //                      Image(systemName: "arrow.right.circle.fill")
-        //                          .font(.title)
-        //                          .foregroundColor(.gray)
-        //                  })
-        //
-        //                Text(Texts.signOut)
-        //                    .foregroundColor(.black)
-        //            }
-        //       }
-        //    }
     }
 }
 
